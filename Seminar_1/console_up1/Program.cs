faa4e28424c096b2664d25e0b02fec0d423fcc6e
Yulia <julsik28@gmail.com>
2023-06-12 15:17:21 +0500

    feat:example and 1exirsice

---
+++ Seminar_1/console_up1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+Console.Write("Введи целое число: ");
+int number = int.Parse(Console.ReadLine()); 
+
+// квадрат входного числа
+int result = number * number;
+
+// вывод результата
+Console.Write($"Квадрат входного числа: {result}");
---
+++ Seminar_1/console_up2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int number3 = number2 * number2;
+
+if (number1 == number3)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else 
+{
+    Console.WriteLine("Не является квадратом");
+}
