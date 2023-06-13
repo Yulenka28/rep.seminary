c35dd707398d4e4f3995be1cf792dff8a787bfc6
Yulia <julsik28@gmail.com>
2023-06-13 23:35:16 +0500

    fix:zadacha1

---
+++ Seminar_2/zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа выводит случайное число из отрезка (10-99) и показывает наибольшую цифру числа
+// Случайное число от 10 до 99 включительно
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Рандомное число: {num}");
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифрв ы числе: {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наименьшая цифра  вчисле: {secondDigit}");
+}
