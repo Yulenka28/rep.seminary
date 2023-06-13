35412b1c6ecea03aa49e780ba283e2ce9a06291d
Yulia <julsik28@gmail.com>
2023-06-13 23:11:14 +0500

    feat:zadacha2

---
+++ Seminar_2/Zadacha2/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 11: Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Рандомное число: {number}");
+
+int result = ((number-number%100)/10+number%10);
+Console.WriteLine($"Итоговое число: {result}");
