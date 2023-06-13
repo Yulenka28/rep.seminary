a9070bc449ae05293dd05f9539de11d79153bdb9
Yulia <julsik28@gmail.com>
2023-06-13 23:45:32 +0500

    feat: add zadacha5_dop

---
+++ Seminar_2/zadacha5_dop/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа на вход принимает число (может быть любой размерности). Необходимо вывести количество цифр в этом числе.
+Console.WriteLine("Введите любое число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+
+while ( num / 10 > 0)
+{
+    count++;
+    num = num / 10;
+}
+Console.WriteLine($"Количество разрядов в числе: {count}");
