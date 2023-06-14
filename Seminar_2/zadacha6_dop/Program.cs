34d3337aa10da5b45ab25b59c3024fed100aa8e1
Yulia <julsik28@gmail.com>
2023-06-14 21:39:31 +0500

    fix: zadacha_6

--- Seminar_2/zadacha6_dop/Program.cs
+++ Seminar_2/zadacha6_dop/Program.cs
@@ -1,22 +1,11 @@
 ﻿// Программа на вход принимает число (может быть любой размерности). Необходимо вывести реверсивное число.
 Console.WriteLine("Введите любое число: ");
-int num = int.Parse(Console.ReadLine());
-int count = 1;
+int a = int.Parse(Console.ReadLine());
+int b = 0;
 
-if (num > 0)
-{
-    while ( num / 10 > 0)
+ while (a != 0)
     {
-        count++;
-        num = num / 10;
+        b = a % 10;
+        a = a / 10;
+        Console.Write(b);
     }
-}
-else
-{
-    while ( num / -10 > 0)
-    {
-        count++;
-        num = num / 10;
-    }
-}
-Console.WriteLine($"Количество разрядов в числе: {count}");
