e1bea4e161d9045bf61746b469e8e7dfd6d45d6f
Yulia <julsik28@gmail.com>
2023-06-14 21:20:18 +0500

    feat: add dz_13

---
+++ Seminar_2/dz_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите любое целое число: ");
+int num = int.Parse(Console.ReadLine());
+
+while (num > 1000)
+{
+    num = num / 10;
+}
+if (num > 99)
+    {
+        int result = num % 10;
+        Console.WriteLine($"Третья цифра: {result}");
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет.");
+    }
