ffa34f625dc2b13403f3cbf585468882f74c767a
Yulia <julsik28@gmail.com>
2023-06-17 00:00:49 +0500

    feat: add zadacha2,3

---
+++ Seminar_3/Zadacha2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти от 1 до 4: ");
+int a = int.Parse(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+    Console.WriteLine("Диапазон x > 0, y > 0");
+    break;
+
+    case 2:
+    Console.WriteLine("Диапазон x > 0, y < 0");
+    break;
+
+    case 3:
+    Console.WriteLine("Диапазон x < 0, y < 0");
+    break;
+
+    case 4:
+    Console.WriteLine("Диапазон x < 0, y > 0");
+    break;
+
+    default: 
+    Console.WriteLine("Вы ввели не верное значение. Значение должно быть от 1 до 4.");
+    break;
+}
---
+++ Seminar_3/Zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int result = 0;
+
+for ( int i = 1; i <= a; i++)
+{
+    result = i * i;
+    Console.Write($"{result} ");
+}
