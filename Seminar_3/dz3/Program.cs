f92a41cae0418730fec54b337f2675e74d4d5d9a
Yulia <julsik28@gmail.com>
2023-06-19 22:19:28 +0500

    feat: add dz3

---
+++ Seminar_3/dz3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+/*int result = 0;
+
+for ( int i = 1; i <= a; i++)
+{
+    result = i * i * i;
+    Console.Write($"{result} ");
+} */
+
+int n = 1;
+while ( n <= a)
+{
+    Console.WriteLine(n * n * n);
+    n++;
+}
