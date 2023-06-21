2372c947eb3eadf639763bebfe2f53f933283ae5
Yulia <julsik28@gmail.com>
2023-06-21 23:03:47 +0500

    feat: add dz1

---
+++ Seminar_4/dz1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+double itog = Math.Pow(a, b);
+Console.WriteLine(itog);
