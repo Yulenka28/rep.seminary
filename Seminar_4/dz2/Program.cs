ba8d9bb1d87496ee64f405d64f31569bbe40c39c
Yulia <julsik28@gmail.com>
2023-06-21 23:19:30 +0500

    feat:add dz2

---
+++ Seminar_4/dz2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите любое число: ");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+
+while (a != 0)
+{
+    b = b + a % 10;
+    a = a / 10;
+}
+Console.WriteLine(b);
