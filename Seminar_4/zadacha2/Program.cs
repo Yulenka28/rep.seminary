266d2e74c6c855609f4a7933803c454db08c9958
Yulia <julsik28@gmail.com>
2023-06-21 22:58:23 +0500

    feat: add zadacha2

---
+++ Seminar_4/zadacha2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+Console.WriteLine("Введите любое число: ");
+int a = int.Parse(Console.ReadLine());
+int n = 1;
+
+for (int i =1; i <= a; i++)
+{
+    n = n * i;
+}
+Console.WriteLine(n);
