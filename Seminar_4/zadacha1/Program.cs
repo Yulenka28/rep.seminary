a37605859eb75006f587fd23e95ae6bfdc2f0208
Yulia <julsik28@gmail.com>
2023-06-21 22:17:10 +0500

    feat: add seminar4

---
+++ Seminar_4/zadacha1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.WriteLine("Введите любое число: ");
+int a = int.Parse(Console.ReadLine());
+int num_0 = Math.Abs(a);
+int i = 1;
+
+while (num_0 > i)
+{
+    num_0 = num_0 / 10;
+    i++;
+}
+Console.WriteLine(i);
