40f219bbab1b5dcc550147c2a4fe4609e863b82e
Yulia <julsik28@gmail.com>
2023-06-26 00:11:49 +0500

    feat:add ZAD4

---
+++ Seminar_5/zad4/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+ второй и предпоследний и т.д. Результат запишите в новом массиве. */
+
+ int[] array = new int[5];
+
+Console.WriteLine("Исходный массив: ");
+for (int i =0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int[] array_new = null;
+if (array.Length % 2 == 0)
+{
+    array_new = new int[array.Length / 2];
+}
+else
+{
+    array_new = new int[array.Length / 2 + 1];
+}
+int index = 0;
+int count = 0;
+int size = array.Length -1;
+int sum = 0;
+while (count <= size)
+{
+    if (count == size)
+    {
+        array_new[index] = array[count];
+        Console.Write(array_new[index] + " ");
+    }
+    else
+    {
+        sum = array[count] * array[size];
+        array_new[index] = sum;
+        Console.Write(array_new[index] + " ");
+    }
+    index++;
+    count++;
+    size--;
+}
