74bae6bd74ebc1b39a44eebde18a1ef5d136f4e3
Yulia <julsik28@gmail.com>
2023-06-25 23:39:21 +0500

    feat:add ZAD2

---
+++ Seminar_5/zad2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+int [] array = new int[7] {5, -1, 3, 7, -9, 12, 44};
+Console.WriteLine("Введите искомое число: ");
+int a = int.Parse(Console.ReadLine());
+
+bool isFound = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == a)
+    {
+        isFound = true;
+        break;
+    }
+}
+Console.WriteLine((isFound ? "Да" : "Нет"));
+
+
+    
