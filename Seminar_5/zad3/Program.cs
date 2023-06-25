a5ff5e8c1c01130e7adde160531232ba0c4162fc
Yulia <julsik28@gmail.com>
2023-06-25 23:28:47 +0500

    feat:add ZAD3

---
+++ Seminar_5/zad3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+int[] array = new int[123];
+
+Console.WriteLine("Исходный массив: ");
+for (int i =0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-1000, 1000);
+    Console.Write(array[i] + " ");
+}
+int quality = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 9 && array[i] < 100)
+    {
+        quality = quality + 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine(quality);
