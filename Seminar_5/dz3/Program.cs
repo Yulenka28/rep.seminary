c402e144b472f16f029ed0257b30d51ff8e01b6a
Yulia <julsik28@gmail.com>
2023-06-26 22:47:24 +0500

    feat:add dz3

---
+++ Seminar_5/dz3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+double[] array = new double[5] {5.6, 1.3, 3, 7.7, 9};
+
+Console.WriteLine("Исходный массив: ");
+for (int i =0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+double max = array[0];
+double min = array[0];
+
+
+for (int i =1; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+
+}
+for (int i =1; i < array.Length; i++)
+{
+    if (min > array[i])
+    {
+        min = array[i];
+    }
+
+}
+double dif = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {dif}");
