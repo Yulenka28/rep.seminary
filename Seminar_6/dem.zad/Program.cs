4770c493b3ed56cb97de933567a59bc93581b0ad
Yulia <julsik28@gmail.com>
2023-06-27 23:00:19 +0500

    feat:add sem6

---
+++ Seminar_6/dem.zad/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу которая перевернет массив.
+
+/*int[] array = new int[6] {1, 4, 7, 2, 75, 21};
+
+int[] reverseArray = new int[array.Length];
+for ( int i = 0; i < reverseArray.Length; i++)
+{
+    reverseArray[i] = array[(array.Length -1) - i];
+}
+
+for ( int i = 0; i < reverseArray.Length; i++)
+{
+    Console.Write(reverseArray[i] + " ");
+}*/
+
+int[] array = new int[6] {1, 4, 7, 2, 75, 21};
+for ( int i = 0; i < array.Length / 2; i++)
+{
+    int temp = array[i];
+    array[i] = array[(array.Length - 1) -i];
+    array[(array.Length - 1) -i] = temp;
+}
+for ( int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
