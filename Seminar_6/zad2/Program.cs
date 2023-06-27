766079cf17c7a81e7a0caa8d277d503e556de363
Yulia <julsik28@gmail.com>
2023-06-28 00:06:24 +0500

    feat:add zad2

---
+++ Seminar_6/zad2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+string result = "";
+
+while (N > 0)
+{
+   result = N % 2 + result;
+   N = N / 2;
+}
+
+Console.WriteLine($"в двоичной системе {result}");
