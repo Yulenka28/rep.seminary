839436e3e316ddd6f469505e98f7b4ad0d76db37
Yulia <julsik28@gmail.com>
2023-07-01 11:01:48 +0500

    feat:add dz1

--- Seminar_6/dz1/Program.cs
+++ Seminar_6/dz1/Program.cs
@@ -1,34 +1,14 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int[] GetRandomArray(int size, int startValue, int endValue)
-{
-    int[] array = new int[size];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(startValue, endValue + 1);
-    }
-    return array;
-}
-
-// Метод вывода массива на экран
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-}
-
-// для вызова методов:
-int[] array = GetRandomArray(10, 100, 1000);
-PrintArray(array);
+Console.Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i] % 2 == 0)
+    if (array[i] > 0)
     {
         count = count + 1;
     }
 }
-Console.WriteLine($"Количество положительных чисел в массиве = {count}");
+Console.WriteLine($"Количество положительных чисел = {count}");
