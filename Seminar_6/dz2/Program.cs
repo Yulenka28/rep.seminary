8aafb3f9d33b4ef5ea1e4ddcacf518633a329b97
Yulia <julsik28@gmail.com>
2023-07-01 11:12:09 +0500

    feat:add dz2

--- Seminar_6/dz2/Program.cs
+++ Seminar_6/dz2/Program.cs
@@ -1,19 +1,20 @@
-﻿// Задача 37: Задайте одномерный массив, заполненный случайными числами. 
-// Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
 
-int[] array = new int[10];
+Console.WriteLine("Введите значение b1 : ");
+double b1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Исходный массив: ");
-for (int i =0; i < array.Length; i++)
-{
-    array[i] = new Random().Next(-99, 100);
-    Console.Write(array[i] + " ");
-}
-Console.WriteLine();
+Console.WriteLine("Введите значение k1 : ");
+double k1 = double.Parse(Console.ReadLine());
 
-int sum = 0;
-for (int i = 1; i < array.Length; i = i + 2)
-{
-    sum = sum + array[i];
-}
-Console.WriteLine($"Сумма элементов на нечетных позициях = {sum}");
+Console.WriteLine("Введите значение b2 : ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2 : ");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
