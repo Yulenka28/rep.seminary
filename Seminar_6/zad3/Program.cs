340b8ba8d05b858b97e3abf688b5871f611edad7
Yulia <julsik28@gmail.com>
2023-07-01 11:24:07 +0500

    feat:add zad3

---
+++ Seminar_6/zad3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+//Первые два числа Фибоначчи: 0 и 1.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int[] fibon = new int[a];
+if ( a > 2)
+{
+    fibon[0] = 0;
+    fibon[1] = 1;
+    for (int i = 2; i < fibon.Length; i++)
+    {
+        fibon[i] = fibon[i - 2] + fibon[i - 1];
+    }
+    for ( int j = 0; j < fibon.Length; j++)
+    {
+        Console.Write(fibon[j] + " ");
+    }
+}
+else
+{
+    Console.Write("Введите число больше 2: первая последовательность будет 0 и 1");
+}
