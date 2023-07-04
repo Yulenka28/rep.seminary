715f7e1bd71a88275944fceea49b1d9bdeb769be
Yulia <julsik28@gmail.com>
2023-07-04 21:38:20 +0500

    feat:add zad1

---
+++ Seminar_7/zad1/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+
+
+int[,] array = new int[3, 4];
+
+for (int i =0; i < 3; i++)
+{
+        for (int j = 0; j < 4; j++)
+        {
+            array[i, j] = i + j;
+        
+        }
+}
+ 
+    for (int i =0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
