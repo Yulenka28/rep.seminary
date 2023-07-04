fb0ee32a294aef3fe967c6926c87e8af5ecdef41
Yulia <julsik28@gmail.com>
2023-07-04 21:06:38 +0500

    feat:add dz2

---
+++ Seminar_7/dz2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0,51);
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>m && b>n)
+{
+    Console.WriteLine("такого числа нет");
+}
+else
+{
+    object c = matrix.GetValue(a,b);
+    Console.WriteLine(c);
+}
