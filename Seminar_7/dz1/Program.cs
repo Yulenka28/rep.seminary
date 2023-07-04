68a5f134e7c50c1a3e068efa9417b7284e7784bd
Yulia <julsik28@gmail.com>
2023-07-04 20:57:06 +0500

    feat:add dz1

---
+++ Seminar_7/dz1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] matrix = new double[3, 4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = Math.Round(new Random().NextDouble(), 2);
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
