41fbebda7dc10516c5ad880d0384be03f7f396a5
Yulia <julsik28@gmail.com>
2023-07-11 18:22:20 +0500

    feat:add dz zad 62

---
+++ Seminar_8/dz3/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07 */
+int n = 4;
+int[,] sqMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqMatrix.GetLength(0) * sqMatrix.GetLength(1))
+{
+  sqMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+PrintArray(sqMatrix);
