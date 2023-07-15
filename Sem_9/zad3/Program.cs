383e866d55e79d992211d206a309580ce56f2904
Yulia <julsik28@gmail.com>
2023-07-15 14:05:20 +0500

    feat:add sem 9 zad 3

---
+++ Sem_9/zad3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 67: Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int Sum (int number)
+{
+    if (number == 0) return 0;
+    int temp =number % 10 + Sum(number / 10);
+    return temp;
+    
+}
+Console.WriteLine(Sum(number));
