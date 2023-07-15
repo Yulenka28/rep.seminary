8ae82b6a481b2dddbd84d2bf929bddf25baaa83a
Yulia <julsik28@gmail.com>
2023-07-15 13:13:17 +0500

    feat:add sem 9 zad 1

---
+++ Sem_9/zad1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int FillNatural(int n)
+{
+    
+    if (n == 0) return 1;
+
+    int temp = 1 + FillNatural(n - 1);
+    Console.Write(" " + n);
+    return temp;
+    
+}
+FillNatural(number);
