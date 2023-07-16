2b8f0acc7f0ebd10b85c14f8b301df54f43961ba
Yulia <julsik28@gmail.com>
2023-07-16 18:41:32 +0500

    feat:add sem 9 dz2

---
+++ Sem_9/dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int Sum (int m, int n)
+{
+    if (m == n) return n;
+    else return m + Sum(m+1, n);
+}
+Console.WriteLine(Sum(m, n));
