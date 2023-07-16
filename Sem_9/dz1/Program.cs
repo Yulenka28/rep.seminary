1f32d590d82417b9278d5ea363725b90d044df28
Yulia <julsik28@gmail.com>
2023-07-16 18:19:36 +0500

    feat:add sem 9 dz1

---
+++ Sem_9/dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int FillNatural(int n)
+{
+    
+    if (n == 0) return 1;
+    Console.Write(" " + n);
+    int temp = 1 + FillNatural(n - 1);
+    
+    return temp;
+    
+}
+FillNatural(number);
